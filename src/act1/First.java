657b0e3ebb8a9de167d69261d7d416720125cb10
RiKs33F <95114554+teten0251@users.noreply.github.com>
2022-02-18 00:46:02 +0900

    add act1

---
+++ src/act1/First.java
@@ -0,0 +1,11 @@
+package act1;
+
+public class First {
+	public static void main(String[] args) {
+		String name = "新人";
+		String hobby = "旅行";
+		
+		System.out.println("名前：" + name);
+		System.out.println("趣味：" + hobby);
+	}
+}
